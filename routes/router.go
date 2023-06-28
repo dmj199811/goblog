3a8f1b73a168750be3b65bbb84baaab20e2ddcd7
stone <441223037@qq.com>
2023-06-28 12:00:21 +0800

    修复icon丢失

--- routes/router.go
+++ routes/router.go
@@ -31,7 +31,7 @@ func InitRouter() {
 
 	r.Static("/static", "./web/front/dist/static")
 	r.Static("/admin", "./web/admin/dist")
-	r.StaticFile("/favicon.ico", "/web/front/dist/favicon.ico")
+	r.StaticFile("/favicon.ico", "./web/front/dist/favicon.ico")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "front", nil)
